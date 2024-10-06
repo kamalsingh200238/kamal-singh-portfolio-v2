f330fb7cc32626d3be173e909c4237e8d1faa8d6
Kamal Singh <kamalsingh200238@gmail.com>
2024-10-07 00:17:07 +0100

    scroll margin and experience section cleanup

--- components/ExperienceSection.vue
+++ components/ExperienceSection.vue
@@ -6,7 +6,7 @@ import { experiences } from "~/data/experiences";
 <template>
   <section
     id="experience"
-    class="py-10 px-8 md:py-12 md:px-12 lg:py-16 lg:px-16 text-slate-300"
+    class="py-10 px-8 md:py-12 md:px-12 lg:py-16 lg:px-16 text-slate-300 scroll-m-16"
   >
     <div class="mx-auto max-w-5xl">
       <h2
@@ -15,8 +15,8 @@ import { experiences } from "~/data/experiences";
         <span class="mr-2 text-teal-300">02.</span> Work Experience
       </h2>
 
-      <Tabs :default-value="experiences[0].company" lass="w-full">
-        <TabsList class="grid w-full grid-cols-2">
+      <Tabs :default-value="experiences[0].company">
+        <TabsList class="grid grid-cols-2">
           <TabsTrigger
             v-for="exp in experiences"
             :key="exp.company"
@@ -30,28 +30,26 @@ import { experiences } from "~/data/experiences";
           :key="exp.company"
           :value="exp.company"
         >
-          <div class="flex flex-col gap-4 lg:flex-row lg:gap-12 mt-4">
-            <div class="w-full lg:w-1/4">
-              <h3 class="mb-2 text-lg font-bold lg:text-xl">
-                {{ exp.position }}
-              </h3>
-              <p class="mb-2 text-lg font-semibold md:text-xl">
-                {{ exp.company }}
-              </p>
-              <p class="mb-4 text-sm text-gray-400 lg:text-base">
-                {{ exp.period }}
-              </p>
-            </div>
-            <div class="w-full lg:w-3/4">
-              <ul class="pl-4 space-y-4 list-disc">
-                <li
-                  v-for="responsibility in exp.responsibilities"
-                  :key="responsibility"
-                >
-                  {{ responsibility }}
-                </li>
-              </ul>
-            </div>
+          <div class="mt-4">
+            <h3 class="mb-2 text-lg font-bold lg:text-xl">
+              {{ exp.position }}
+            </h3>
+            <p class="mb-2 text-lg font-semibold md:text-xl text-teal-300">
+              {{ exp.company }}
+            </p>
+            <p class="mb-4 text-sm lg:text-base text-teal-300">
+              {{ exp.period }}
+            </p>
+          </div>
+          <div>
+            <ul class="pl-4 space-y-4 list-disc">
+              <li
+                v-for="responsibility in exp.responsibilities"
+                :key="responsibility"
+              >
+                {{ responsibility }}
+              </li>
+            </ul>
           </div>
         </TabsContent>
       </Tabs>
