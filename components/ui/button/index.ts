10ee867d8100803d3d41a78b689d3b8f3b8ff9ce
Kamal Singh <kamalsingh200238@gmail.com>
2024-10-06 23:24:31 +0100

    add new variant and customize already existing varaint

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -11,11 +11,12 @@ export const buttonVariants = cva(
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+          'text-teal-300 border border-input bg-background hover:bg-primary hover:text-primary-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        link: 'text-teal-300 hover:text-teal-500',
+        icon: '[&svg]:w-full [&svg]:h-auto bg-transparent text-teal-300 hover:text-teal-500',
       },
       size: {
         default: 'h-10 px-4 py-2',
