d248b7b7ca85e2004f2a297bb77f4b539d9e47c3
Kamal Singh <kamalsingh200238@gmail.com>
2024-10-07 21:43:01 +0100

    add fontawesome properly

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [ '@nuxtjs/tailwindcss','shadcn-nuxt'],
-  css: ['~/assets/css/tailwind.css'],
+  css: [
+    '@fortawesome/fontawesome-svg-core/styles.css'
+    ,'~/assets/css/tailwind.css'
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
