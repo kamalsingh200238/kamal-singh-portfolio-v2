1d0d90ee3dbfb66b11d33801c84ef63eca9dec8b
Kamal Singh <kamalsingh200238@gmail.com>
2024-10-15 22:11:23 +0100

    fix font awesome errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: [ '@nuxtjs/tailwindcss','shadcn-nuxt'],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
   css: [
-    '@fortawesome/fontawesome-svg-core/styles.css'
-    ,'~/assets/css/tailwind.css'
+    "@fortawesome/fontawesome-svg-core/styles.css",
+    "~/assets/css/tailwind.css",
   ],
+  build: {
+    transpile: [
+      "@fortawesome/fontawesome-svg-core",
+      "@fortawesome/free-brands-svg-icons",
+      "@fortawesome/free-regular-svg-icons",
+      "@fortawesome/free-solid-svg-icons",
+      "@fortawesome/vue-fontawesome",
+    ],
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -14,7 +23,7 @@ export default defineNuxtConfig({
     },
   },
   shadcn: {
-    prefix: '',
-    componentDir: './components/ui'
+    prefix: "",
+    componentDir: "./components/ui",
   },
-})
+});
