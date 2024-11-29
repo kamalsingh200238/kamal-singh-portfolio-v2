ff758bd438d932b0ac04ef47517bd24d58895386
Kamal Singh <kamalsingh200238@gmail.com>
2024-11-29 19:02:45 +0000

    remove border from project image

--- components/ProjectSection.vue
+++ components/ProjectSection.vue
@@ -44,7 +44,7 @@ import { projects } from "~/data/projects.js";
                   <img
                     :src="image.src"
                     :alt="image.alt"
-                    class="object-contain rounded-lg w-full aspect-video border border-teal-800"
+                    class="object-contain rounded-lg w-full aspect-video"
                   />
                 </CarouselItem>
               </CarouselContent>
